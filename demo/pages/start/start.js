029deeba5b93ed7324f824cb6cb1eea9a300d648
luguanxing <651571925@qq.com>
2018-06-28 20:54:59 +0800

    删除扫码中转界面

--- demo/pages/start/start.js
+++ demo/pages/start/start.js
@@ -38,9 +38,20 @@ Page({
   },
   toCust: function () {
     var that = this;
-    wx.navigateTo({
-      url: '../saoMa/saoMa'
-      //url: '../customerMainPage/customerMainPage'
+    wx.scanCode({
+      success: (res) => {
+        console.log(res);
+        var url = res.result;
+        var username = url.substring(url.indexOf('=') + 1, url.indexOf('&'));
+        var tablenum = url.substring(url.lastIndexOf('=') + 1);
+        that.setData({
+          username: username,
+          tablenum: tablenum
+        });
+        wx.navigateTo({
+          url: '../customerMainPage/customerMainPage?username=' + that.data.username + '&tablenum=' + that.data.tablenum
+        })
+      }
     })
   }
 })
